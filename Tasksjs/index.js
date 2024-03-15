f17ebff8a42152362b361a8f5fd2075c491a28f7
Ivanzolo2007 <akboltaib@gmail.com>
2024-03-15 12:40:29 +0500

    hw

---
+++ Tasksjs/index.js
@@ -0,0 +1,8 @@
+function addVariable(a, b) {
+  return a + b;
+}
+
+const a = 5;
+const b = 5;
+
+console.log(addVariable(a, b));
---
+++ Tasksjs/index2.js
@@ -0,0 +1,16 @@
+const names = ["Владилен", "Елена", "Игорь", "Ксения"];
+
+// names.push("Алена")
+// names.unshift("Алена")
+
+// const firstName = names.shift()
+// const lastName = names.pop()
+
+// console.log(names.reverse())
+// const reversed = names.toReversed();
+
+// const letters = ["e", "c", "d", "b", "a"];
+// console.log(letters.toSorted());
+// console.log(letters);
+
+console.log(names.splice(0, 2));
